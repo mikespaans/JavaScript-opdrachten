fbbff3d067933f01388f492329c5061d54e8fc3d
mike spaans <99069233@mydavinci.nl>
2023-03-10 14:25:53 +0100

    personenregister opdracht 2 afgemaakt

--- personen_register/personen.js
+++ personen_register/personen.js
@@ -1,12 +1,14 @@
 let Main = document.getElementById("main");
 let ObjectArray = ["voornaam", "achternaam", "nationaliteit", "leeftijd", "gewicht"]
-fetch('opdracht1_bijlage.json')
+fetch('opdracht2_bijlage.json')
     .then(response => response.json())
     .then(data => {
         // Do something with the JSON data
-        for (let info of ObjectArray) {
-            console.log(data[info]);
-            Main.innerHTML += data[info] + "<br>";
+        for (let i = 0; i < data.length; i++) {
+            for (let info of ObjectArray) {
+
+                Main.innerHTML += data[i][info] + "<br>";
+            }
         }
     })
     .catch(error => console.error(error));
